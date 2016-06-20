0673ece0225a96889e0a523b4243fa6dd50a2017
technet <ellipse.paro@gmail.com>
2016-06-20 23:57:37 +0200

    setup 2

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name paintfusionApp
+ * @description
+ * # paintfusionApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('paintfusionApp', []);
