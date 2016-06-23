fa6685cd1c423355bdce0d61f7997acc50844977
technet <ellipse.paro@gmail.com>
2016-06-23 13:36:22 +0200

    feat(login): server select

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -13,7 +13,18 @@ angular.module('paintfusionApp')
     $scope.showMail = false;
     $scope.pwdReco = function() {
       $scope.showMail = !$scope.showMail;
-
     };
+    $scope.serverTab = [
+      {"id": "euw", "name": "Europe west"},
+      {"id": "na", "name": "North America"},
+      {"id": "kr", "name": "Korea"},
+      {"id": "eune", "name": "Europe NOrth-East"},
+      {"id": "br", "name": "Brazil"},
+      {"id": "lan", "name": "Europe west"},
+      {"id": "las", "name": "Europe west"},
+      {"id": "tr", "name": "Turkey"},
+      {"id": "oce", "name": "Oceanie"},
+
+    ]
 
   });
