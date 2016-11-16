685471c0ef07b2948bcf45e5eef2c18ec265823f
technet <ellipse.paro@gmail.com>
2016-11-16 08:41:14 +0100

    feat(history):barely working

--- app/scripts/controllers/historyController.js
+++ app/scripts/controllers/historyController.js
@@ -2,14 +2,17 @@
 
 angular.module('paintfusionApp')
   .controller('HistoryCtrl', function($scope, $state, $stateParams, apiService, autreService) {
+    $scope.isHistoryAvailable = 0;
+    //$scope.$watch (function(){ return {'sumId':$scope.sumId, 'server':$scope.server};}, function () {$scope.myParams = 1;});
 
-    console.log('lol');
-/*
-    apiService.getHistorique($scope.sumId,$scope.server).then(function(hist){
-      console.log(hist.stats.games);
-      $scope.games = hist.stats.games;
-    });
-
+    $scope.historyRequired = function(){
+    if ($scope.sumId && $scope.server){
+      apiService.getHistorique($scope.sumId,$scope.server).then(function(hist){
+        $scope.games = hist.stats.games;
+      });
+    $scope.isHistoryAvailable = 1;
+    }
+  };
 
     $scope.summonerSpell = function (id){
       return autreService.resolveSummonerSpell(id);
@@ -17,6 +20,6 @@ angular.module('paintfusionApp')
     $scope.champion = function (id){
       return autreService.resolveChampion(id);
     };
-*/
+
   });
 
---
+++ app/scripts/controllers/navBar.js
@@ -0,0 +1,3 @@
+/**
+ * Created by moran on 16/11/2016.
+ */
